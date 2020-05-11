be3f5e68711f239d92ce20d998df491ffd15a7d4
Jayden Thunberg <jlthunberg@gmail.com>
2020-05-11 09:49:15 -0500

    added table to admin

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,16 +1,22 @@
 import React, { Component } from "react";
-import {Button} from '@material-ui/core/';
+import { Button } from '@material-ui/core/';
 // import axios from 'axios';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
 
 
 
-class Admin extends Component{
+class Admin extends Component {
 
     state = {
         feedbackList: [],
 
-      }
-    handleClick = () =>{
+    }
+    handleClick = () => {
         console.log('in handleclick admin')
         // axios.get('/feedback')
         // .then((response) => {
@@ -25,8 +31,8 @@ class Admin extends Component{
         // })
     }
 
-    render(){
-        return(
+    render() {
+        return (
             <div>
                 <h1>Welcome to Admin</h1>
 
@@ -34,7 +40,34 @@ class Admin extends Component{
 
                 <p align='center'>Right now I only append to "http://localhost:5000/feedback", try again later and my link may be working.</p>
 
-                <Button size="small"variant="outlined" color="primary" onClick={this.handleClick}>Get Feedback</Button>
+                <Button size="small" variant="outlined" color="primary" onClick={this.handleClick}>Get Feedback</Button>
+
+                <TableContainer>
+                    <Table aria-label="simple table">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>New Feedback)</TableCell>
+                                <TableCell align="right">Feeling</TableCell>
+                                <TableCell align="right">Support&nbsp;</TableCell>
+                                <TableCell align="right">Understanding&nbsp;</TableCell>
+                                <TableCell align="right">Comments&nbsp;</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        {/* <TableBody>
+          {rows.map((row) => (
+            <TableRow key={row.name}>
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="right">{row.feeling}</TableCell>
+              <TableCell align="right">{row.support}</TableCell>
+              <TableCell align="right">{row.understanding}</TableCell>
+              <TableCell align="right">{row.comments}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody> */}
+                    </Table>
+                </TableContainer>
 
             </div>
         ) //end return
